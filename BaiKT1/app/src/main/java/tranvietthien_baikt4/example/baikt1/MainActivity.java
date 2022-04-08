8c8014b105e327b60d4499ad4703fc18f56daba4
tranthien727 <tran.thien727@gmail.com>
2022-04-08 16:16:03 +0700

    Bài tập lấy điểm bài 1

---
+++ BaiKT1/app/src/main/java/tranvietthien_baikt4/example/baikt1/MainActivity.java
@@ -0,0 +1,17 @@
+package tranvietthien_baikt4.example.baikt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
