6b7fcb038eb05619cef957001e0c6af552ee3189
tranthien727 <tran.thien727@gmail.com>
2022-04-08 16:46:05 +0700

    Bai tập lấy điểm bài 1

--- BaiKT1/app/src/main/java/tranvietthien_baikt4/example/baikt1/MainActivity.java
+++ BaiKT1/app/src/main/java/tranvietthien_baikt4/example/baikt1/MainActivity.java
@@ -6,12 +6,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtName,edtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void DangNhap(View v){
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+
+        if(edtName.getText().toString().equals("maicuongtho") && edtPass.getText().toString().equals("Cntt60ntu!"))
+        {
+            Toast.makeText(this, "User and Password is correct", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "User and Password is wrong", Toast.LENGTH_SHORT).show();
+        }
     }
 }
