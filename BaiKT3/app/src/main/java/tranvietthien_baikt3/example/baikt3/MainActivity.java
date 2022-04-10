3f44ae385dc18260bb70014d9a8a1235c88dec80
tranthien727 <tran.thien727@gmail.com>
2022-04-10 13:18:28 +0700

    Bài tập lấy điểm bài 3

---
+++ BaiKT3/app/src/main/java/tranvietthien_baikt3/example/baikt3/MainActivity.java
@@ -0,0 +1,14 @@
+package tranvietthien_baikt3.example.baikt3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
